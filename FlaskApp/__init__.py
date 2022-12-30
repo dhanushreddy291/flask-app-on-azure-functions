41522bcdfcccae0e4a583562b32f180d42937d49
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-12-30 17:21:28 +0530

    Remove Files

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -5,20 +5,9 @@ from .package.module import MODULE_VALUE
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return (
-        "Try /hello/Chris for parameterized Flask route.\n"
-        "Try /module for module import guidance"
-    )
-
-@app.route("/hello/<name>", methods=['GET'])
-def hello(name: str):
-    return f"hello {name}"
-
-@app.route("/module")
-def module():
-    return f"loaded from FlaskApp.package.module = {MODULE_VALUE}"
+    return "A Web App with Python (Flask) on Microsoft Azure Functions!"
 
 if __name__ == "__main__":
     app.run()
